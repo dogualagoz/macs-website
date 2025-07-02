a9a81e7f0a50db02ca42d9d4c2f2de858bc24d74
dogu <alagozdogu@gmail.com>
2025-07-02 19:30:10 +0300

    events model completed

---
+++ backend/models/events.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Column, Integer, Boolean, String, Text, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from database import Base
+
+class Event(Base):
+
+    __tablename__ = "events"
+
+    #Primary key
+    id = Column(Integer, primary_key=True, index=True)
+
+    #Bilgiler 
+    title = Column(String, nullable=False)
+    slug = Column(String, unique=True, nullable=False)
+    description = Column(String)
+    content = Column(Text)
+    image_url = Column(String)
+    location = Column(String)
+
+    #Zamanlar
+    start_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=False)
+
+    # İlişkiler
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    created_by = Column(Integer, ForeignKey("users.id"))
+
+    # Durum
+    is_active = Column(Boolean, default=True)
+    is_deleted = Column(Boolean, default=False)
+    
+    # Timestamps
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+class EventCategory(Base):
+    __tablename__ = "event_categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+
+    #Timestamps
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
