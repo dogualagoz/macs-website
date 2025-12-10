0721a238409db52cd65179d2a893624652a9b885
dogu <alagozdogu@gmail.com>
2025-12-10 23:18:17 +0300

    MACS Website 1.6.0 feat(Sponsors):Sponsor page is finished -feat(admin): update button and form is added. -coming soon page is added

---
+++ frontend/src/shared/components/pages/ComingSoon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../../styles/pages/comingsoon.css';
+
+const ComingSoon = () => (
+	<div className="coming-soon-container">
+		<h1>Çok Yakında!</h1>
+		<p>Sponsorluk sayfamız üzerinde çalışıyoruz. Çok yakında burada olacak!</p>
+	</div>
+);
+
+export default ComingSoon;
